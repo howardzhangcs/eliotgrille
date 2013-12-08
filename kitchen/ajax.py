05d2181515401ca249fd8821537ffc395e61f979
Howard Zhang <howardzhang@college.harvard.edu>
2013-12-08 01:49:11 -0500

    buggy implementation of dajaxice - latest version

--- kitchen/ajax.py
+++ kitchen/ajax.py
@@ -1,8 +1,13 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+from django.forms import CreateAddForm
 
 @dajaxice_register
-def sayhello(request):
-    return simplejson.dumps({'message':'Hello World'})
-
-
+def temp_add_order(request, form):
+    add_form = CreateAddForm(deserialize_form(form))
+    if add_form.is_valid():
+	    p = add_form.cleaned_data['order_food']
+        n = add_form.cleaned_data['order_quantity']	
+	    m = Order(order_food = p, order_quantity = n, is_currentorder = True, dateofentry = timezone.now())
+        latest_order_list = Order.objects.get(is_create=True)
+        return simplejson.dumps({'latest_order_list': latest_order_list})
